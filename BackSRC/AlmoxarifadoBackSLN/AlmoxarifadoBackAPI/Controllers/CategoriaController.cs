adadd5ea2df1b62036a8f60d718edfb73d9903d3
ruanmiguel15 <168589982+ruanmiguel15@users.noreply.github.com>
2025-02-07 13:42:43 -0300

    vt

--- BackSRC/AlmoxarifadoBackSLN/AlmoxarifadoBackAPI/Controllers/CategoriaController.cs
+++ BackSRC/AlmoxarifadoBackSLN/AlmoxarifadoBackAPI/Controllers/CategoriaController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace AlmoxarifadoBackAPI.Controllers
 {
     [Route("[controller]")]
-    [ApiController]
+    [ApiController] 
     public class CategoriaController : ControllerBase
     {
         private readonly ICategoriaRepositorio _db;
